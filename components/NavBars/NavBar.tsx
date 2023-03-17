b79b9cf459526539959b4207e7dd08cb8087537f
Michał Michalik <86044526+michalik-michal@users.noreply.github.com>
2023-03-17 18:38:09 +0100

    Use next/link instead of a

--- components/NavBars/NavBar.tsx
+++ components/NavBars/NavBar.tsx
@@ -1,12 +1,13 @@
-import { Link } from "react-scroll";
+import { Link as ScrollLink } from "react-scroll";
+import Link from 'next/link'
 
 export default function NavBar() {
     return (
           <div className=" bg-zinc-900 text-white h-16 flex flex-row-reverse px-16 sm:text-xl fixed w-screen"> 
-          <button className='px-5 hover:font-bold'> <a href="/aboutme"> About me</a></button>
-           <button className='px-5 hover:font-bold'> <Link to="my-projects" smooth={true} duration={400} offset={-150}> My Projects</Link></button>
-            <button className='px-5 hover:font-bold'> <Link to="experience" smooth={true} duration={400} offset={-150}> Experience</Link></button>
-            <button className='px-5 hover:font-bold'> <Link to="home" smooth={true} duration={500}> Home</Link></button>
+          <button className='px-5 hover:font-bold'> <Link href="/aboutme"> About me</Link></button>
+           <button className='px-5 hover:font-bold'> <ScrollLink to="my-projects" smooth={true} duration={400} offset={-150}> My Projects</ScrollLink></button>
+            <button className='px-5 hover:font-bold'> <ScrollLink to="experience" smooth={true} duration={400} offset={-150}> Experience</ScrollLink></button>
+            <button className='px-5 hover:font-bold'> <ScrollLink to="home" smooth={true} duration={500}> Home</ScrollLink></button>
           </div>
       )
   }
