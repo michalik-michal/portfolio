f3cc10dac07737ea553dd92ee9a3703ceab75cef
Michał Michalik <86044526+michalik-michal@users.noreply.github.com>
2023-03-17 17:17:10 +0100

    Replace video

--- pages/aboutme.tsx
+++ pages/aboutme.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
     <header className=" flex h-full overflow-hidden relative">
         <AboutMeSection/>
-        <video  src="/MICHAL1.mp4" autoPlay muted className=" absolute z-10 w-auto min-w-full min-h-full max-w-none brightness-50"/>
+        <video  src="Michal.mp4" autoPlay muted className=" absolute z-10 w-auto min-w-full min-h-full max-w-none brightness-50"/>
     </header>
 
     )
