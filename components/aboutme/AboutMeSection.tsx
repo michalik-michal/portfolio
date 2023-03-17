fac4904f471368b9b9dfbc6dfcee99334c348490
Michał Michalik <86044526+michalik-michal@users.noreply.github.com>
2023-03-17 17:18:26 +0100

    Include download link

--- components/aboutme/AboutMeSection.tsx
+++ components/aboutme/AboutMeSection.tsx
@@ -17,7 +17,7 @@ export default function AboutMeSection() {
                                 <h1 className="text-3xl sm:text-5xl font-bold pl-4 pt-4 "> Michal Michalik </h1>
                             </div>
                             <h2 className="text-xl sm:text-3xl sm:w-[40rem] sm:leading-relaxed pb-5"> I am an ex athlete. I was a long time member of the Polish Alpine Skiing National Team. I started coding around 2020 and I quit professional sport in the beggining of 2022 to start working as software engeneer.</h2>
-                            {/* <a href="public/cliftonstrengthsMichalMichalik.pdf" download="newfilename" className="text-l sm:text-xl bg-gray-500 p-4 rounded-md hover:font-semibold"> See My Clifton Strengths Test ✏️ </a> */}
+                            <a href="cliftonstrengthsMichalMichalik.pdf" download="Michal Michalik" className="text-l sm:text-xl bg-gray-500 p-4 rounded-md hover:font-semibold"> See My Clifton Strengths Test ✏️ </a>
                         </div>
                     </div>
                 </div>
