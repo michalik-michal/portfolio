61a7f71825681d19f3cfd499fa515d697705e9ef
Michał Michalik <86044526+michalik-michal@users.noreply.github.com>
2023-03-19 09:52:55 +0100

    fix navbar alignment

--- components/NavBars/NavBar.jsx
+++ components/NavBars/NavBar.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export default function NavBar() {
     return (
-          <div className=" bg-zinc-900 text-white h-16 flex flex-row-reverse px-16 sm:text-xl fixed w-screen"> 
+          <div className=" bg-zinc-900 text-white h-16 flex flex-row-reverse sm:px-16 sm:text-xl fixed w-screen"> 
           <button className='px-5 hover:font-bold'> <Link href="/aboutme"> About me</Link></button>
            <button className='px-5 hover:font-bold'> <ScrollLink to="my-projects" smooth={true} duration={400} offset={-150}> My Projects</ScrollLink></button>
             <button className='px-5 hover:font-bold'> <ScrollLink to="experience" smooth={true} duration={400} offset={-150}> Experience</ScrollLink></button>
