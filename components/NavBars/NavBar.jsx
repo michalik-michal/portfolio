81fc19d45c1c30a65d3905d72fd0ba594cf28d42
Michał Michalik <86044526+michalik-michal@users.noreply.github.com>
2023-07-30 13:53:38 +0200

    Add upwork link

--- components/NavBars/NavBar.jsx
+++ components/NavBars/NavBar.jsx
@@ -5,6 +5,7 @@ export default function NavBar() {
     return (
           <div className=" bg-zinc-900 text-white h-16 flex flex-row-reverse sm:px-16 sm:text-xl fixed w-screen"> 
           <button className='px-5 hover:font-bold'> <Link href="/aboutme"> About me</Link></button>
+          <button className='px-5 hover:font-bold'> <Link href="https://www.upwork.com/freelancers/~0170e04352719544aa?viewMode=1"> Hire Me</Link></button>
            <button className='px-5 hover:font-bold'> <ScrollLink to="my-projects" smooth={true} duration={400} offset={-150}> My Projects</ScrollLink></button>
             <button className='px-5 hover:font-bold'> <ScrollLink to="experience" smooth={true} duration={400} offset={-150}> Experience</ScrollLink></button>
             <button className='px-5 hover:font-bold'> <ScrollLink to="home" smooth={true} duration={500}> Home</ScrollLink></button>
