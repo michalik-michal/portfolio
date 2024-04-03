e5866b816a0503f898b5e8cb00bd755e5614a648
Michał Michalik <86044526+michalik-michal@users.noreply.github.com>
2024-04-03 17:08:49 +0200

    Add nitch privacy policy

---
+++ pages/nitchpolicy.tsx
@@ -0,0 +1,8 @@
+const PDFViewer = () => {
+    return (
+    <div className=" w-screen h-screen">
+        <iframe src='/NitchPrivacyPolicy.pdf' width='100%' height='100%' />
+    </div>
+    );
+   };
+   export default PDFViewer;
