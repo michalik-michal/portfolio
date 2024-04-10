04a870a6346c897bb1584f58230adc8f1d24d51a
Michał Michalik <86044526+michalik-michal@users.noreply.github.com>
2024-04-10 10:19:32 +0200

    fix build

--- components/common/wavyBackground.tsx
+++ components/common/wavyBackground.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { cn } from "components/utils/cn";
+import { cn } from "@/components/utils/cn";
 import React, { useEffect, useRef, useState } from "react";
 import { createNoise3D } from "simplex-noise";
  
