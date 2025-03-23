2c5952c28789eaf773927ad332eb0d646035474c
Michal Michalik <michalik515@gmail.com>
2025-03-23 20:57:12 +0100

    home

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,29 +12,42 @@ import QuickMath from "@/components/myprojects/QuickMath";
 import Head from "next/head";
 import AmwaySection from "@/components/experience/amwaySection";
 import Nitch from "@/components/myprojects/Nitch";
-
+import { TextHoverEffect } from "@/components/common/text-hover-effect";
+import { TbMail } from "react-icons/tb";
+import { toast, Toaster } from "react-hot-toast";
 
 export default function Home() {
+  const handleEmailClick = () => {
+    navigator.clipboard.writeText('michalpiotr.michalik@gmail.com');
+    toast.success("Email copied to clipboard");
+  };
+
   return (
-      <div>
-        <NavBar/>
-        <Head>
-        <link rel="shortcut icon" href="favicon.png" />
-        <title> Michał Michalik </title>
-        </Head>
-        <div className='bg-black text-white'>
-          <SocialIcons/>
-          <ExperienceHeader/>
-          <AmwaySection/>
-          <FootballCoSection/>
-          <MyProjectsHeader/>
-          <Nitch/>
-          <SkiDay/>
-          <QuickMath/>
-          <SignIn/>
-          <MoreProjectsButton/>
-          <Footer/>
+    <div className="h-screen w-full flex flex-col overflow-hidden">
+      <Toaster position="top-center" />
+      
+      <main className="flex-1 flex items-center justify-center">
+        <div style={{ width: '100vw', height: '90vh', maxWidth: '2400px' }}>
+          <TextHoverEffect 
+            text="i'ts coming..." 
+            width="100%" 
+            height="100%" 
+            viewBox="0 0 100 70"
+            fontSize="30vw"
+            duration={0.2}
+          />
         </div>
-      </div>
-    )
+      </main>
+      
+      <footer className="flex flex-col items-center py-6">
+        <button 
+          onClick={handleEmailClick} 
+          className="flex items-center gap-2 text-gray-400 hover:text-gray-300 transition-colors duration-300"
+        >
+          <TbMail className="w-6 h-6" />
+          <span className="text-lg">michalpiotr.michalik@gmail.com</span>
+        </button>
+      </footer>
+    </div>
+  );
 }
