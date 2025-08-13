e97215c3ae444df9a91558395d28f9a32c167f0d
Michal Michalik <michalik515@gmail.com>
2025-08-13 08:24:43 +0200

    CP

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,13 +19,16 @@ export default function Home() {
 
   return (
     <div className="min-h-screen w-full flex items-center justify-center">
-      <button
-        onClick={handleCopy}
-        className="flex items-center gap-3 text-gray-300 hover:text-gray-100 transition-colors text-2xl focus:outline-none"
-        type="button"
-      >
-        <span className="text-gray-400 hover:text-gray-100 transition-colors duration-300">{email}</span>
-      </button>
+      <div className="flex flex-col items-center">
+        <button
+          onClick={handleCopy}
+          className="flex items-center gap-3 text-gray-300 hover:text-gray-100 transition-colors text-2xl focus:outline-none"
+          type="button"
+        >
+          <span className="text-gray-400 hover:text-gray-100 transition-colors duration-300">{email}</span>
+        </button>
+        <p className="text-gray-400 mt-2">Mobile App Development</p>
+      </div>
       <Toaster />
     </div>
   );
