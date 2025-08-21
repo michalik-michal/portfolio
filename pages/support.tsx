c29962ef4d2d95c75d00c3ed75e28b654ebde5d9
Michal Michalik <michalik515@gmail.com>
2025-08-21 14:54:51 +0200

    Support Page

---
+++ pages/support.tsx
@@ -0,0 +1,19 @@
+export default function Support() {
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center bg-gray-900">
+      <div className="p-8">
+        <iframe 
+          src="https://docs.google.com/forms/d/e/1FAIpQLSeIc9uhZ1M9Qt7SPw4I_1H-YXuoTngRcfFxZvlM3-LAg5M0YQ/viewform?embedded=true" 
+          width={640} 
+          height={419} 
+          frameBorder="0" 
+          marginHeight={0} 
+          marginWidth={0}
+          title="Support Form"
+        >
+          Ładuję…
+        </iframe>
+      </div>
+    </div>
+  );
+}
